d67e87f4e023a9ec841ca822e6ae73ad38d3af98
Till Meyer zu Westram <till.meyerzuwestram@zuehlke.com>
2021-10-13 11:01:31 +0200

    print json in api errors if available

--- netilion/error.py
+++ netilion/error.py
@@ -1,5 +1,28 @@
+from typing import Optional
+
+from requests import Response
+
+
 class GenericNetilionApiError(Exception):
-    pass
+    __response: Optional[Response] = None
+
+    def __init__(self, response: Optional[Response] = None) -> None:
+        if response:
+            super().__init__(response)
+        else:
+            super().__init__()
+        self.__response = response
+
+    def __str__(self) -> str:
+        if self.__response is not None:
+            if hasattr(self.__response, "headers") and \
+                    isinstance(self.__response.headers, dict) and \
+                    'application/json' in self.__response.headers.get('Content-Type'):
+                return f"{self.__class__.__name__}: {self.__response.json()}"
+            else:
+                return f"{self.__class__.__name__}: {self.__response}"
+        else:
+            return super().__str__()
 
 
 class MalformedNetilionApiResponse(GenericNetilionApiError):
---
+++ tests/test_error.py
@@ -0,0 +1,38 @@
+from netilion.error import *
+from requests import Response
+
+
+class TestError:
+    def test_generic_error_without_message(self):
+        err = GenericNetilionApiError()
+        assert str(err) == ''
+
+    def test_generic_error_with_json_response(self):
+        resp = Response()
+        resp.status_code = 400
+        resp.headers = {"Content-Type": "application/json"}
+        resp._content = b'{"errors": [{"type": "error_type"}]}'
+
+        err = GenericNetilionApiError(resp)
+
+        assert str(err) == "GenericNetilionApiError: {'errors': [{'type': 'error_type'}]}"
+
+    def test_generic_error_with_non_json_response(self):
+        resp = Response()
+        resp.status_code = 400
+        resp.headers = {"Content-Type": "application/xml"}
+        resp._content = b'<html></html>'
+
+        err = GenericNetilionApiError(resp)
+
+        assert str(err) == "GenericNetilionApiError: <Response [400]>"
+
+    def test_malformed_api_response_with_json_response(self):
+        resp = Response()
+        resp.status_code = 400
+        resp.headers = {"Content-Type": "application/json"}
+        resp._content = b'{"errors": [{"type": "error_type"}]}'
+
+        err = MalformedNetilionApiResponse(resp)
+
+        assert str(err) == "MalformedNetilionApiResponse: {'errors': [{'type': 'error_type'}]}"
